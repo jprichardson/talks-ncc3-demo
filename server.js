d947b9588d4e4d50764a09a4839434a3197e5a4b
JP Richardson <jprichardson@gmail.com>
2013-03-12 01:10:33 -0500

    install express, serve up some html

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log("hello!")
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+
+var app = express()
+
+var clientDir = path.join(__dirname, 'client')
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000)
+  app.use(express.logger('dev'))
+  app.use(express.bodyParser()) //parses json, multi-part (file), url-encoded
+  app.use(app.router) //need to be explicit, (automatically adds it if you forget)
+  app.use(express.static(clientDir)) //should cache static assets
+})
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+})
+
+app.get('/', function(req, res) {
+  res.sendfile(path.join(clientDir, 'index.html'))
+})
+
+var server = http.createServer(app)
+
+server.listen(app.get('port'), function(){
+  console.log("Web server listening on port " + app.get('port'));
+});
+
+
