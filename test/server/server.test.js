d91762011d723f6e60642521aef2d67ccc7f1647
JP Richardson <jprichardson@gmail.com>
2013-03-13 00:04:56 -0500

    install mocha

---
+++ test/server/server.test.js
@@ -0,0 +1,16 @@
+var testutil = require('testutil')
+  , sa = require('superagent')
+
+var PORT = 3000
+  , URL = 'http://localhost:' + PORT
+
+
+describe('server', function() {
+  it('should connect to the default web page and grab the content', function(done) {
+    sa.get(URL).end(function(resp) {
+      //console.log(resp.text)
+      T (resp.text.indexOf('Hello!') > 0)
+      done()
+    })
+  })
+})
