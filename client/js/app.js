5e802cddc5eb010dd935954b2df135030c67e96b
JP Richardson <jprichardson@gmail.com>
2013-03-15 18:22:52 -0500

    directives in forms

--- client/js/app.js
+++ client/js/app.js
@@ -42,6 +42,16 @@ CarApp.filter('range', function() {
   };
 })
 
+CarApp.directive('formfield', function() {
+  return {
+    restrict: 'E', //could be E, A, C (class), M (comment)
+    scope: {
+      prop: '@',
+      data: '=ngModel'
+    },
+    templateUrl: '/partials/formfield.html'
+  }
+})
 
 
 
